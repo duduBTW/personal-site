fafe35ef5aa5bd6355056c5b1179cb5cdfcf8448
Carlos Eduardo Alves do Nascimento <carloseduardo108090@hotmail.com>
2021-07-24 11:44:02 -0300

    projet images

--- src/pages/project/index.tsx
+++ src/pages/project/index.tsx
@@ -1,14 +1,19 @@
 import { GetStaticProps } from "next";
 import { NextSeo } from "next-seo";
+import { OpenGraphImages } from "next-seo/lib/types";
 import ProjectsTemplate from "templates/Projects/Projects";
 import { IProject } from "types/Project";
 import { getData } from "./[projectid]";
 
 interface Props {
   projects: IProject[];
+  imagesOG: OpenGraphImages[];
 }
 
-export default function ProjectsPage({ projects }: Props): JSX.Element {
+export default function ProjectsPage({
+  projects,
+  imagesOG,
+}: Props): JSX.Element {
   return (
     <>
       <NextSeo
@@ -19,6 +24,12 @@ export default function ProjectsPage({ projects }: Props): JSX.Element {
           site: "@dudubtway",
           cardType: "summary_large_image",
         }}
+        openGraph={{
+          title: `Projetos`,
+          description: `Dudubtw, desenvolvedor frontend projetos`,
+          site_name: "DuduBTW",
+          images: imagesOG,
+        }}
       />
       <ProjectsTemplate projects={projects} />
     </>
@@ -28,9 +39,14 @@ export default function ProjectsPage({ projects }: Props): JSX.Element {
 export const getStaticProps: GetStaticProps<any, any> = async (props) => {
   const data: IProject[] = await getData();
 
+  const imagesOG: OpenGraphImages[] = data.map(
+    (item): OpenGraphImages => ({ url: item.miniature })
+  );
+
   return {
     props: {
       projects: data,
+      imagesOG,
     },
   };
 };
